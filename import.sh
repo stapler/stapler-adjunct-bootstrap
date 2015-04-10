f43602c9358a84813896d69e40d4270de50aa9ed
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-10 16:01:19 -0700

    Bootstrap2 to bootstrap3 was a major change, so it deserves a new artifact name
    (This way bootstrap2 and bootstrap3 can co-exist in the same VM.)

--- import.sh
+++ import.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -ex
-wget -O bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
+wget -O bootstrap.zip https://github.com/twbs/bootstrap/archive/v3.3.4.zip
 zip=$PWD/bootstrap.zip
 cd src/main/bootstrap/org/kohsuke/stapler
-rm -rf bootstrap
+rm -rf *
 unzip $zip
+mkdir bootstrap3
+D=$PWD/bootstrap3
+
+# move only the stuff we care --- dist & less
+pushd bootstrap-*
+  mv dist/* $D
+  mv less $D
+popd
+rm -rf bootstrap-*
